1f093e8f9c230d9ed9239476a67bf705902099c1
MilanPatel2003 <milan.patel@esparkbizmail.com>
2026-06-05 18:57:28 +0530

    bugFix: path fixed superadmin route.ts

--- src/modules/superAdmin/superadmin.routes.ts
+++ src/modules/superAdmin/superadmin.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import tenantRoutes from "./routes/tenant.routes";
-import superAdminAuthRoutes from "./routes/tenant.routes";
+import superAdminAuthRoutes from "./routes/superAdmin.auth.routes";
 
 
 const router = express.Router();
