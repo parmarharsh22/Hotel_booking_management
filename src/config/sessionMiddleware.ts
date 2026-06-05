cac63a1becbe3592dc099cf1c4dd5e7a7aa9160c
manthan <manthanj005@gmail.com>
2026-06-05 10:38:16 +0530

    files deleted

--- src/config/sessionMiddleware.ts
+++
@@ -1,7 +0,0 @@
-import session  from "express-session";
-
-export const sessionMiddleware = session({
-    secret: process.env.SESSION_KEY || 'NOTHING',
-    resave:false,
-    saveUninitialized:false
-})
