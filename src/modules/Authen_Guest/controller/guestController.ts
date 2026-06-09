57a613d762139a98d71f610886c44df2a1e619b5
parmarharsh22 <harshparmar01051986@gmail.com>
2026-06-09 18:25:28 +0530

    Added the editProfile superAdmin admin

--- src/modules/Authen_Guest/controller/guestController.ts
+++ src/modules/Authen_Guest/controller/guestController.ts
@@ -1,6 +1,7 @@
 import { Request,Response } from "express";
 import { getJwtTokenValue } from "../../../common/utils/getRequestVariables";
 import * as guestServices from "../services/guestServices";
+import { deleteOldPhoto } from "../../../common/utils/deleteOldProfilePic";
 
 //Get the user details
 export const getUserDetails = async(req:Request,res:Response) =>{
@@ -11,4 +12,30 @@ export const getUserDetails = async(req:Request,res:Response) =>{
     }catch(err: any){
         return res.status(500).send(err.message);
     }
+}
+
+//Update the user Profile
+export const updateProfile = async(req:Request,res:Response)=>{
+    try{
+        const id = getJwtTokenValue("userId",req);
+        const data = await guestServices.getUserData(id);
+        
+        await guestServices.updateUser(id,req);
+        
+        //remove old photo
+        if(req.file?.filename){
+            deleteOldPhoto(data.photo_url);
+        }
+        
+        (req.session as any).profileEdited = "Profile Updated successfully!"
+        res.redirect("/");
+    }catch(err:any){
+        return res.status(500).send(err.message);
+    }
+}
+
+//logout a user
+export const logoutUser = async(req:Request,res:Response)=>{
+    res.clearCookie("token");
+    return res.redirect("/");
 }
--- src/modules/Authen_Guest/services/guestServices.ts
+++ src/modules/Authen_Guest/services/guestServices.ts
@@ -1,9 +1,21 @@
 import * as guestModel from "../models/guestModels";
 
+//get user data from the db
 export const getUserData = async(userId: string)=>{
     const data = await guestModel.getUserDetails(userId);
     if(!data){
         throw new Error("No such user exists");
     }
     return data;
+}
+
+//update the user data
+export const updateUser = async(userId: string,req:any)=>{
+    if(!userId){
+        throw new Error("Invalid Id specified");
+    }
+    const fileName = req.file?.filename;
+    const {first_name,last_name,phone,dob,gender,address,city,state}= req.body
+    const user_id = userId;
+    await guestModel.updateUserDetails(userId,first_name,last_name,phone,dob,gender,address,city,state,fileName);
 }
